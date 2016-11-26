b9aae25d770c734e9bcefcc267ce38e864eb86ab
andreadelrio <delrio.andre@gmail.com>
2016-11-26 00:30:28 -0800

    18. persisting our state with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyA9VhI08C_vBwyNHwg7yXD2HUIyIgl16Cs",
+  authDomain: "catch-of-the-day-adr.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-adr.firebaseio.com",
+});
+
+export default base;
